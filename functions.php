84752c0ce3eda709554b14b62a0fa746be37c5cc
Ivan Rey De Cardo <ivanreydecardo@gmail.com>
2018-04-14 21:20:01 +0400

    The hint of a design

--- functions.php
+++ functions.php
@@ -1,6 +1,10 @@
 <?php
 
 function theme_enqueue_assets() {
+    wp_enqueue_style('google_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('theme_styles', get_stylesheet_uri());
+
+    wp_enqueue_script('theme_scripts', get_theme_file_uri('/js/scripts-bundled.js'), null, '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'theme_enqueue_assets');
