2f759956c80a1305e656ac4b63d1cb76bd993e3e
Ivan Rey De Cardo <ivanreydecardo@gmail.com>
2018-04-15 21:14:25 +0400

    The homepage blog posts

--- functions.php
+++ functions.php
@@ -47,3 +47,40 @@ function get_page_parent_permalink($id = null) {
 function get_page_parent_title($id = null) {
     return get_the_title($id ?: get_page_parent_id());
 }
+
+function the_homepage_blog_posts($args = []) {
+    $query = new WP_Query(array_merge([
+        'post_type' => 'post',
+        'posts_per_page' => 2,
+    ], $args));
+
+    $html = '';
+
+    while ($query->have_posts()) {
+        $query->the_post();
+
+        $html .= '<div class="event-summary">
+            <a class="event-summary__date event-summary__date--beige t-center" href="' . get_the_permalink() . '">
+                <span class="event-summary__month">' . get_the_time('M') . '</span>
+                <span class="event-summary__day">' . get_the_time('d') . '</span>
+            </a>
+
+            <div class="event-summary__content">
+                <h5 class="event-summary__title headline headline--tiny">
+                    <a href="' . get_the_permalink() . '">
+                        ' . get_the_title() . '
+                    </a>
+                </h5>
+
+                <p>
+                    ' . wp_trim_words(get_the_excerpt(), 15) . '
+                    <a href="' . get_the_permalink() . '" class="nu gray">Read more</a>
+                </p>
+            </div>
+        </div>';
+    }
+
+    wp_reset_postdata();
+
+    echo $html;
+}
